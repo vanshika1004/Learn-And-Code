a637df56fb645a4159fbfc244218149a22f78898
vanshika <vanshikagupta10804@gmail.com>
2025-12-19 12:51:52 +0530

    Examples for SOID Principles added

---
+++ Week2/SOLID principles/SolidPrinciples/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_principles
+{
+    internal class InterfaceSegregationPrinciple
+    {
+        public static void Run()
+        {
+            IPrinter printer = new SimplePrinter();
+            IScanner scanner = new SimpleScanner();
+
+            printer.Print("Hello ISP");
+            scanner.Scan();
+
+        }
+    }
+    //ISP - Interface Segregation Principle - A class should not be forced to implement methods it does not use.
+    interface IPrinter
+    {
+        void Print(string content);
+    }
+
+    interface IScanner
+    {
+        void Scan();
+    }
+
+    // Only prints, not forced to implement Scan
+    class SimplePrinter : IPrinter
+    {
+        public void Print(string content)
+        {
+            Console.WriteLine($"Printing: {content}");
+        }
+    }
+
+    // Only scans, not forced to implement Print
+    class SimpleScanner : IScanner
+    {
+        public void Scan()
+        {
+            Console.WriteLine("Scanning document...");
+        }
+    }
+}
---
+++ Week2/SOLID principles/SolidPrinciples/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_principles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program started For SRP\nSRP - Single Responsibility Principle - This principle says a class should have only one reason to change.");
+            SRP.Run();
+            Console.WriteLine("Program ended For SRP\n\n");
+
+            Console.WriteLine("Program started For OCP\nOCP- Open Closed Principle - Open for Extension, Closed for Modification");
+            OCP.Run();
+            Console.WriteLine("Program ended For OCP\n\n");
+
+            Console.WriteLine("Program started For LSP\nLSP - Liskov Substitution Principle - Subclasses should be substitutable for their base class without breaking behavior.");
+            LSP.Run();
+            Console.WriteLine("Program ended For LSP\n\n");
+
+            Console.WriteLine("Program started For ISP\nISP - Interface Segregation Principle - A class should not be forced to implement methods it does not use.");
+            InterfaceSegregationPrinciple.Run();
+            Console.WriteLine("Program ended For ISP\n\n");
+
+            Console.WriteLine("Program started For DIP\nDIP - Dependency Inversion Principle - High‑level modules depend on abstractions, not on concrete classes.");
+            InterfaceSegregationPrinciple.Run();
+            Console.WriteLine("Program ended For DIP\n\n");
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine(); 
+        }
+    }
+}
