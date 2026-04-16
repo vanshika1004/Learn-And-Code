9e734675f2f97c46a628a72af5dd82d4ca56ae3f
vanshika <vanshikagupta10804@gmail.com>
2026-04-16 22:37:30 +0530

    Added Unit Tests Assignment

---
+++ Chapter 9/DivisorAssignment/DivisorAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DivisorAssignment.Services;
+
+namespace DivisorAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            int testCaseCount = int.Parse(Console.ReadLine()!);
+
+            IDivisorService divisorService = new DivisorService();
+
+            while (testCaseCount-- > 0)
+            {
+                int maxNumber = int.Parse(Console.ReadLine()!);
+
+                int result = divisorService.CountNumbersWithEqualAdjacentDivisors(maxNumber);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ Chapter 9/DivisorAssignment/DivisorAssignment/Services/DivisorService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisorAssignment.Services
+{
+        public class DivisorService : IDivisorService
+        {
+            public int CountNumbersWithEqualAdjacentDivisors(int maxNumber)
+            {
+                if (maxNumber <= 2)
+                    return 0;
+
+                int[] divisorCounts = new int[maxNumber + 1];
+
+                for (int currentDivisor = 1; currentDivisor <= maxNumber; currentDivisor++)
+                {
+                    for (int multiple = currentDivisor; multiple <= maxNumber; multiple += currentDivisor)
+                    {
+                        divisorCounts[multiple]++;
+                    }
+                }
+
+                int validCount = 0;
+
+                for (int currentNumber = 2; currentNumber < maxNumber; currentNumber++)
+                {
+                    if (divisorCounts[currentNumber] == divisorCounts[currentNumber + 1])
+                    {
+                        validCount++;
+                    }
+                }
+
+                return validCount;
+            }
+        }
+}
