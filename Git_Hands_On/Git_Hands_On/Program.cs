df943e9093372df80a421cf837e591e3dfc1a386
vanshika <vanshikagupta10804@gmail.com>
2026-04-30 06:13:00 +0530

    v4 bug introduced

--- Git_Hands_On/Git_Hands_On/Program.cs
+++ Git_Hands_On/Git_Hands_On/Program.cs
@@ -7,5 +7,6 @@ class Program
         Console.WriteLine("v1 working");
         Console.WriteLine("v2 working");
         Console.WriteLine("v3 working");
+        throw new Exception("bug introduced");
     }
 }
